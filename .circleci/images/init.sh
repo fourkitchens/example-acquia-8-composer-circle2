758fa7a26ccec80bc36c9a5cad0df5df1309e06c
Allan Chappell <generalredneck@gmail.com>
2018-08-18 17:14:29 -0500

    Initial Commit of circleci 2.0 configuration.

---
+++ .circleci/images/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo service mysql start
+sudo service apache2 start
+bash
---
+++ .circleci/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'project',
+  'username' => 'project',
+  'password' => 'project',
+  'host' => 'localhost',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
