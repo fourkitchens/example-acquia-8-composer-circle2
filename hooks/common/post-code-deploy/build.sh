09b4ca163f1c10c01ca5ed5e9ba41c3d45b6c7a8
Allan Chappell <generalredneck@gmail.com>
2018-08-21 09:55:20 -0500

    Adding deployment hooks.

---
+++ hooks/common/post-code-deploy/build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Cloud Hook (common): post-code-update
+#
+# The post-code-update hook runs in response to code commits.
+# When you push commits to a Git branch, the post-code-update hooks runs for
+# each environment that is currently running that branch. See
+# ../README.md for details.
+#
+# Usage: post-code-update site target-env source-branch deployed-tag repo-url
+#                         repo-type
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+# You must set up the following variables in
+# /mnt/gfs/home/$site/$target_env/nobackup/bashkeys.sh
+#
+# - `ACQUIA_USER`
+#    Cloud API E-mail
+# - `ACQUIA_TOKEN`
+#    Cloud API Private key. Located in your profile under security.
+#    See: https://docs.acquia.com/acquia-cloud/api/auth
+
+ACQUIA_ENDPOINT="https://cloudapi.acquia.com/v1"
+ACQUIA_CANONICAL_ENV="test"
+# Grab Keys
+# @see https://docs.acquia.com/acquia-cloud/files/system-files/private
+source /mnt/gfs/home/$site/$target_env/nobackup/bashkeys.sh
+
+if [ -z "$ACQUIA_USER" ]
+then
+  echo "There are no drush keys set up for this environment."
+  exit 1;
+else
+  drush @$site.$target_env ac-api-login --email=$ACQUIA_USER --key=$ACQUIA_TOKEN --endpoint=$ACQUIA_ENDPOINT
+fi
+
+if [ -f /mnt/gfs/home/$site/$target_env/nobackup/skipbuild ]
+then
+  echo "The skip file was detected. You must run backups and build commands manually."
+  exit 0;
+fi
+
+helper_script_dir="$( dirname $0 )/../../../scripts/acquia"
+
+# Backing up current environment.
+$helper_script_dir/wait_for_cloud_done.sh @$site.$target_env ac-database-instance-backup $site
+
+# If we aren't on the cononical env, pull in cononical's db and files
+if [ "$target_env" != "$ACQUIA_CANONICAL_ENV" ]
+then
+  $helper_script_dir/wait_for_cloud_done.sh @$site.$ACQUIA_CANONICAL_ENV ac-database-copy $site $target_env
+  $helper_script_dir/wait_for_cloud_done.sh @$site.$ACQUIA_CANONICAL_ENV ac-files-copy $target_env
+fi
+
+echo "Starting Build for $target_env"
+if [ "$target_env" = 'dev' ]
+then
+  # If we are on dev, we want development tools enabled so we can work.
+  drush @$site.$target_env local
+else
+  # Otherwise, all the other environments are built the same (including prod)
+  drush @$site.$target_env prod
+fi
+echo "Ending Build for $target_env"
---
+++ scripts/acquia/wait_for_cloud_done.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Runs an acquia task, and waits for the task to complete before continuing.
+# This is a helper script, to be used in others as needed.
+#
+# @see https://gist.github.com/generalredneck/44875680786c6ca6cd6713aa5e5adb7d
+
+if [[ $1 = "" ]] || [[ $2 = "" ]]
+then
+  echo "Runs an acquia drush command, waiting for the results before continuing."
+  echo "Can be used as a replacement for drush."
+  echo
+  echo "    Usage:    $0 <site-alias> <ac-drush-command>"
+  echo "    Example:  $0 @site.prod ac-database-instance-backup site"
+  exit 1
+fi
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+ac_command="$2"
+drush_command="drush "$@""
+
+if [[ $ac_command = "ac-code-deploy" ]] ||
+   [[ $ac_command = "ac-code-path-deploy" ]] ||
+   [[ $ac_command = "ac-database-add" ]] ||
+   [[ $ac_command = "ac-database-copy" ]] ||
+   [[ $ac_command = "ac-database-delete" ]] ||
+   [[ $ac_command = "ac-database-instance-backup" ]] ||
+   [[ $ac_command = "ac-database-instance-backup-delete" ]] ||
+   [[ $ac_command = "ac-database-instance-backup-restore" ]] ||
+   [[ $ac_command = "ac-domain-add" ]] ||
+   [[ $ac_command = "ac-domain-delete" ]] ||
+   [[ $ac_command = "ac-domain-move" ]] ||
+   [[ $ac_command = "ac-domain-purge" ]] ||
+   [[ $ac_command = "ac-environment-install" ]] ||
+   [[ $ac_command = "ac-files-copy" ]]
+then
+  echo "${0//.sh/}:"
+  echo "    Command: $drush_command"
+  jsonfile=$(mktemp /tmp/wait_for_cloud_done.XXXXXX)
+  $drush_command --format=json > $jsonfile
+  id="$( php -r "\$json = json_decode(file_get_contents('$jsonfile')); echo \$json->id;" )"
+
+  echo "    Task: $id"
+  rm $jsonfile;
+
+  oldlogs=""
+  COUNTER=0
+  # Hard timeout at 10 minutes
+  while [[ $state != "done" && $state != "error"  && $COUNTER -lt 60 ]]
+  do
+    # Checking consumes resources, so wait for 10 seconds between checks.
+    sleep 10
+    let COUNTER=COUNTER+1
+    jsonfile=$(mktemp /tmp/wait_for_cloud_done.XXXXXX)
+    drush $1 ac-task-info $id --format=json > $jsonfile
+    if [[ $? -gt 0 ]]
+    then
+      echo "drush ac-task-info Command Failed"
+      exit 1
+    elif [[ -z "$(cat $jsonfile)" ]]
+    then
+      echo "drush ac-task-info didn't return any data."
+    fi
+    state="$( php -r "\$json = json_decode(file_get_contents('$jsonfile')); echo \$json->state;" )"
+    newlogs="$( php -r "\$json = json_decode(file_get_contents('$jsonfile')); echo \$json->logs;" )"
+    rm $jsonfile
+    if [[ $newlogs != $oldlogs ]]
+    then
+      logdiff=${newlogs//"$oldlogs"/}
+      logdiff=${logdiff//"\n"/}
+      if [[ $logdiff != "" ]]
+      then
+        echo "    $logdiff"
+      fi
+    fi
+    oldlogs="$newlogs"
+  done
+
+  if [[ $state != "error" ]]
+  then
+    echo -e "    State: ${GREEN}$state${NC}"
+  else
+    echo -e "    State: ${RED}$state${NC}"
+    exit 1
+  fi
+else
+  # Fall back to standard drush command if this is not a known asyncronous command
+  drush "$@"
+fi
